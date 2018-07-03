36f78243526395cfbf167313e3eefd83e0eab48a
Chouyou Yang <chouyouyang@Chouyous-MacBook-Pro.local>
2018-07-03 10:41:20 -0500

    Initial Setup

---
+++ CustomLists/CustomListsTest/CustomLists<T>.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+namespace CustomListsTest
+{
+    [TestFixture()]
+    public class CustomLists<T>
+    {
+        [Test()]
+        public void AddtoListCustom()
+        {   
+
+            // Arrange
+            CustomLists<T> myList = new CustomLists<T>();
+
+            int value = 16;
+
+
+            // Act
+            myList.Add(value);
+
+            // Assert
+            Assert.AreEqual(value, myList[0]);
+
+        }
+    }
+}
